f9a731bff8ebae0547c35fb2310ea4d2824453f3
AjmalCybro <ajmal@cybrosys.in>
2026-08-21 17:30:36 +0530

    Aug 20 [ADD] Initial Commit

---
+++ odoo_nhs_governance/__manifest__.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2026-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'NHS Governance Management',
+    'version': '19.0.1.0.0',
+    'category': 'Healthcare/NHS',
+    'summary': 'NHS board, committee and governance management with meetings, actions and assurance.',
+    'description': """NHS Governance Management provides a centralised platform for managing NHS boards and committees,
+including terms of reference, membership, meeting schedules, agendas, papers, minutes, and actions.
+It also supports declarations of interest and the Board Assurance Framework (BAF), linking strategic
+risks, controls, assurances, and actions to support effective governance and oversight.
+    """,
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base','mail','odoo_nhs_trust_management'],
+    'data': [
+        'security/nhs_governance_security.xml',
+        'security/ir.model.access.csv',
+        'data/nhs_gov_sequence_data.xml',
+        'data/nhs_gov_committee_type_data.xml',
+        'data/nhs_gov_interest_category_data.xml',
+        'data/nhs_gov_assurance_line_data.xml',
+        'data/nhs_gov_month_data.xml',
+        'data/mail_template_data.xml',
+        'data/ir_cron_data.xml',
+        'wizards/nhs_meeting_generate_wizard_views.xml',
+        'wizards/nhs_agenda_from_cycle_wizard_views.xml',
+        'wizards/nhs_board_pack_wizard_views.xml',
+        'views/nhs_committee_views.xml',
+        'views/nhs_director_views.xml',
+        'views/nhs_meeting_views.xml',
+        'views/nhs_agenda_item_views.xml',
+        'views/nhs_cycle_of_business_views.xml',
+        'views/nhs_meeting_action_views.xml',
+        'views/nhs_declaration_views.xml',
+        'views/nhs_baf_views.xml',
+        'views/nhs_governance_calendar_views.xml',
+        'views/nhs_governance_dashboard_views.xml',
+        'views/nhs_governance_config_views.xml',
+        'report/nhs_board_pack_report.xml',
+        'report/nhs_baf_report.xml',
+        'report/nhs_doi_register_report.xml',
+        'report/nhs_action_log_report.xml',
+        'views/nhs_governance_menus.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'odoo_nhs_governance/static/src/dashboard/governance_dashboard.js',
+            'odoo_nhs_governance/static/src/dashboard/governance_dashboard.xml',
+            'odoo_nhs_governance/static/src/dashboard/governance_dashboard.scss',
+        ],
+        'web.assets_backend_lazy': [
+            'odoo_nhs_governance/static/src/baf_pivot_color/baf_pivot_color.js',
+            'odoo_nhs_governance/static/src/baf_pivot_color/baf_pivot_color.xml',
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
---
+++ odoo_nhs_governance/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <odoo_nhs_governance>
+
+#### 21.08.2026
+#### Version 19.0.1.0.0
+#### ADD
+
+- Initial release of NHS Governance Management.
+- NHS board, committee and governance management with meetings, actions and assurance.
